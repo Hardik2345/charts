75ed404b4db2ff5cdc7b674234026a75bde1edba
Hardik Parikh <hardikparikh19@gmail.com>
2025-01-24 10:37:36 +0530

    Fix line endings

--- mycharts/src/App.js
+++ mycharts/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dataCard revenueCard">Chart 1</div>
+
+      <div className="dataCard cutomerCard">Chart 2</div>
+
+      <div className="dataCard categoryCard">Chart 3</div>
     </div>
   );
 }
