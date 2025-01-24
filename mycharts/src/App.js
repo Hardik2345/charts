5d5ebb5d83f509349e53b463b19c13131555394c
Hardik Parikh <hardikparikh19@gmail.com>
2025-01-24 11:20:42 +0530

    container created

--- mycharts/src/App.js
+++ mycharts/src/App.js
@@ -1,11 +1,23 @@
 function App() {
   return (
     <div className="App">
-      <div className="dataCard revenueCard">Chart 1</div>
-
-      <div className="dataCard cutomerCard">Chart 2</div>
-
-      <div className="dataCard categoryCard">Chart 3</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center", // Centers horizontally
+          alignItems: "center", // Centers vertically
+          height: "100vh", // Makes the parent fill the viewport
+        }}
+      >
+        <div
+          className="card text-bg-light my-3"
+          style={{ minWidth: "60rem", minHeight: "35rem" }}
+        >
+          <div className="card-body">
+            <h5 className="card-title">Chart</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
